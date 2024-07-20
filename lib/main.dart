0d3b1a1d0919fda77b505bf42d1a87f0725219a7
Ahmed Halim <theoosirisov@gmail.com>
2024-07-21 00:33:30 +0300

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:sports_app/screens/leagues_screen.dart';
 
 void main() {
-  runApp( LeaguesScreen(countryKey:44,));
+  runApp( LeaguesScreen(countryKey:42,));
 }
